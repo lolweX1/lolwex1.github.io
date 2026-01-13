d6262ed5200eefff527363ca47e52eda6adc2c14
Jeffrey Fu <jeffreyfu99@gmail.com>
2026-01-13 11:03:13 -0600

    edit

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const tabs = {};
-const navBar = document.getElementById('nav_bar');
+const navBar = document.getElementById("nav_bar");
 
 for (let child of navBar.children) {
-  const key = child.id.replace('_tab', '');
-  tabs[key] = child;
+  const key = child.id.replace("_tab", "");
+  tabs[key] = document.getElementById(key);
+}
+
+function setActiveTab(tabKey) {
+  for (let key in tabs) {
+    tabs[key].classList.remove("active");
+    document.getElementById(key + "_tab").classList.remove("active_nav");
+  }
+  tabs[tabKey].classList.add("active");
+  document.getElementById(tabKey + "_tab").classList.add("active_nav");
 }
